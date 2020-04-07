b104b158dee33015a6f984392816646f60554325
dondropo <alejandroruscamoreno@gmail.com>
2020-04-07 18:48:28 +0000

    adding libraries and defining holberton.h library

---
+++ holberton.h
@@ -0,0 +1,17 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+
+/*LIBRARIES*/
+#include <stdio.h>
+#include <sdtlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <signals.h>
+
+/*FUNCTIONS*/
+extern char **environ;
+
+#endif
